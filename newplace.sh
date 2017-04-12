48ae9959e6a0b4e0878d95fcc37f7bd60456ed0c
remotephone <remotephone@gmail.com>
2017-04-11 21:45:59 -0500

    1st version - hope it works

---
+++ newplace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## This will get a new VM or whatever set up like I like it. 
+
+## Get some colors
+echo "Making color dir..."
+mkdir ~/.vim/
+mkdir ~/.vim/colors
+
+echo "Getting colors..."
+curl https://raw.githubusercontent.com/raphamorim/lucario/master/colors/lucario.vim -o ~/.vim/colors/lucario.vim
+
+## Install Vundle
+
+echo "Creating the bundle dir and getting Vundle..."
+mkdir ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+## Get my .vimrc
+
+echo "Getting the vimrc..."
+curl https://raw.githubusercontent.com/remotephone/configs/master/.vimrc -o ~/.vimrc
+
+##Install plugins
+echo "Installing plugins...."
+vim +PluginInstall +qall
+
+echo "And we're done!"
